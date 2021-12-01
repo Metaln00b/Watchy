a9ddecc3b3f602b2e386dd493a83c3ccc4ddb79d
Metaln00b <Metaln00b@web.de>
2021-12-01 23:44:21 +0100

    7_SEG: Fair fixing of wednesday display issue
    Spotted at https://github.com/LeeHolmes/watchysim

--- examples/WatchFaces/7_SEG/Watchy_7_SEG.cpp
+++ examples/WatchFaces/7_SEG/Watchy_7_SEG.cpp
@@ -51,6 +51,10 @@ void Watchy7SEG::drawDate(){
     uint16_t w, h;
 
     String dayOfWeek = dayStr(currentTime.Wday);
+    if (currentTime.Wday == 4)
+    {
+        dayOfWeek = "Wednesdy";
+    }
     display.getTextBounds(dayOfWeek, 5, 85, &x1, &y1, &w, &h);
     if(currentTime.Wday == 4){
         w = w - 5;
